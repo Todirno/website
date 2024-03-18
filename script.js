54dc853430871dff3ed983a7e6e6e78b56b30aeb
Todirno <74773497+Todirno@users.noreply.github.com>
2024-03-18 18:20:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {scrollFunction()};
+            
+function scrollFunction() {
+  var scrollButton = document.getElementById("scrollButtonBottom");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+    scrollButton.style.opacity = "1";
+  } else {
+    scrollButton.style.opacity = "0";
+    setTimeout(function(){ scrollButton.style.display = "none"; }, 100);
+  }
+}
+
+function scrollToTop() {
+  const scrollDuration = 1000;
+  const scrollStep = -window.scrollY / (scrollDuration / 70);
+  const scrollInterval = setInterval(function() {
+    if (window.scrollY != 0) {
+      window.scrollBy(0, scrollStep);
+    } else clearInterval(scrollInterval);
+  }, 15);
+}
